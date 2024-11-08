c9bf16534c0c3009364372865fad9327ca51b759
mertakman <mertakman2012@gmail.com>
2024-11-08 00:28:29 +0000

    add: dlq query params

--- examples/events/main.rs
+++ examples/events/main.rs
@@ -5,13 +5,10 @@ use qstash_rs::{client::QstashClient, events_types::EventsRequest};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = env::var("QSTASH_API_KEY").expect("QSTASH_API_KEY not set");
-
-    let client = QstashClient::builder()
-        .api_key(&api_key)
-        .build()
-        .unwrap();
+    let client = QstashClient::builder().api_key(&api_key).build().unwrap();
 
     let resp = client.list_events(EventsRequest::default()).await?;
+
     println!("{:#?}", resp);
     Ok(())
 }
